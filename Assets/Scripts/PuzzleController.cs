f6f8ae210a634dd5d2675a6251ee96a43a24701c
DenkenAn <amanthukral2002@gmail.com>
2024-03-07 10:25:19 -0800

    Puzzles added

---
+++ Assets/Scripts/PuzzleController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public GameObject button5;
+    public GameObject button6;
+    public GameObject button7;
+    public GameObject button8;
+
+    public GameObject puzzle;
+
+    private GameObject[] buttons;
+    private bool isSolved;
+    private string[] answer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttons = new GameObject[8]{button1, button2, button3, button4, button5, button6, button7, button8};
+        answer = new string[8] {"unselected", "selected", "unselected", "unselected", "unselected", "unselected", "unselected", "unselected"};
+        isSolved = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool curAnswer = true;
+        for (int i = 0; i < 8; i++) {
+            curAnswer = curAnswer && buttons[i].CompareTag(answer[i]);
+        }
+        isSolved = curAnswer;
+
+        if (isSolved) {
+            puzzle.SetActive(false);
+        }
+    }
+}
