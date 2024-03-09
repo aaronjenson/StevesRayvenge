8f6fee1f7a1a43dc61193bd6a44708a0a8a553c1
Aaron Jenson <aaronajenson@gmail.com>
2024-03-09 15:20:00 -0800

    added main menu

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -2,19 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject rayPrefab;
     public GameObject rightControllerGameObject;
+    public List<GameObject> disableOnGameStart;
+    public List<GameObject> enableOnGameStart;
+    public bool rayShootingEnabled = true;
     private InputDevice rightController;
     private bool rayGenerated;
+    private List<TeleportationArea> _teleportationAreas;
 
     void Start()
     {
         rayGenerated = false;
+        rayShootingEnabled = false;
         List<InputDevice> devices = new List<InputDevice>();
         InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
         InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
@@ -22,13 +29,24 @@ public class NewBehaviourScript : MonoBehaviour
         if (devices.Count > 0) {
             rightController = devices[0];
         }
+
+        _teleportationAreas = new List<TeleportationArea>();
+        foreach(GameObject obj in FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+        {
+            TeleportationArea comp = obj.GetComponent<TeleportationArea>();
+            if (comp != null)
+            {
+                _teleportationAreas.Add(comp);
+                comp.enabled = false;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         rightController.TryGetFeatureValue(CommonUsages.trigger, out float rightTriggerValue);
-        if (rightTriggerValue > 0.5f) {
+        if (rightTriggerValue > 0.5f && rayShootingEnabled) {
             if (!rayGenerated) {
                 Instantiate(rayPrefab, rightControllerGameObject.transform.position, rightControllerGameObject.transform.rotation * Quaternion.Euler(90, 0, 0));
                 rayGenerated = true;
@@ -36,5 +54,23 @@ public class NewBehaviourScript : MonoBehaviour
         } else {
             rayGenerated = false;
         }
+    }
+
+    public void GameStarted()
+    {
+        EnableRayShooting();
+        _teleportationAreas.ForEach(area => area.enabled = true);
+        enableOnGameStart.ForEach(obj => obj.SetActive(true));
+        disableOnGameStart.ForEach(obj => obj.SetActive(false));
+    }
+
+    public void EnableRayShooting()
+    {
+        rayShootingEnabled = true;
+    }
+
+    public void DisableRayShooting()
+    {
+        rayShootingEnabled = false;
     }
 }
